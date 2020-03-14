73974e7f5d50cd707663ee5af2854ee3d5a4e9a8
markorencia <orencia.mark@outlook.com>
2020-03-14 10:54:53 +0800

    Nginx base installation for puppetlabs

---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+# @summary
+#   Allows for the Nginx service to restart when triggered
+class nginx::service {
+  service { "${nginx::service_name}":
+    alias      => 'nginx_service',
+    ensure     => $nginx::service_ensure,
+    enable     => $nginx::service_enable,
+    hasrestart => true,
+  }
+}
+
